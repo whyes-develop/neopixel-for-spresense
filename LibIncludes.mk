4e30f88c01dc496b1d707d807436b10601a35116
whyes <whyes@whyesdevelop.myguest.virtualbox.org>
2023-06-12 21:28:12 +0900

    Add copyright

--- LibIncludes.mk
+++ LibIncludes.mk
@@ -1,3 +1,6 @@
+# SPDX-FileCopyrightText: 2023 whyes-develop
+# SPDX-License-Identifier: MIT
+
 ifeq ($(CONFIG_EXTERNALS_NEOPIXEL),y)
 NEOPIXEL_LIBDIR += $(EXTERNAL_DIR)$(DELIM)neopixel-for-spresense$(DELIM)neopixel_extlib
 CFLAGS     += ${shell $(INCDIR) $(INCDIROPT) "$(CC)" "$(NEOPIXEL_LIBDIR)"}
